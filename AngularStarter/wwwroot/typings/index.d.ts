54136d6c1561c847bd112bd3e5c41f92d16b1e59
ProMatrix <david.donovan@promatrixinc.com>
2017-10-08 03:53:57 -0500

    Create index.d.ts

---
+++ AngularStarter/wwwroot/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/file-saver/filesaver.d.ts" />
+/// <reference path="globals/lodash/lodash.d.ts" />
+/// <reference path="../../node_modules/moment/moment.d.ts" />
